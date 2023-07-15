d3f2f412ce02babcd095cf54e7a1497cc5d5014e
Alcimar Costa <alcimar.zeneide@gmail.com>
2023-07-15 20:20:16 -0300

    conclusao da implementação da classe Modelo

--- src/main/java/domain/Modelo.java
+++ src/main/java/domain/Modelo.java
@@ -1,4 +1,57 @@
 package domain;
 
+import java.util.Objects;
+
 public class Modelo {
+
+
+    private Long id;
+    private Marca marca;
+    private String descricao;
+
+    public Modelo() {
+    }
+
+    public Modelo(Long id, Marca marca, String descricao) {
+        this.id = id;
+        this.marca = marca;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Modelo modelo)) return false;
+        return Objects.equals(id, modelo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
