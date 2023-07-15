fd3a230c101f4ba7e86a23a47fd0f1f9fffa80c6
Alcimar Costa <alcimar.zeneide@gmail.com>
2023-07-15 20:14:25 -0300

    Concluindo a classe Cor

--- src/main/java/domain/Cor.java
+++ src/main/java/domain/Cor.java
@@ -4,4 +4,42 @@ import java.util.Objects;
 
 public class Cor {
 
+    private Long id;
+    private String descricao;
+
+    public Cor() {
+    }
+
+    public Cor(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cor cor)) return false;
+        return Objects.equals(id, cor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
