676ffb8194ee4a8ab7fd0c245cca5a420d14cff5
Alcimar Costa <alcimar.zeneide@gmail.com>
2023-07-15 20:46:41 -0300

    Implementando a execução do sistema de cadastro de veículos

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,2 +1,23 @@
-package controller;public class Main {
+package controller;
+
+import domain.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cor cor = new Cor(1L, "Azul");
+        Marca marca = new Marca(1L, "Chevrolet");
+        Modelo modelo = new Modelo(1L, marca, "Camaro");
+        TipoVeiculo tipoVeiculo = new TipoVeiculo(1L, "Carro");
+
+        Veiculo veiculo = new Veiculo();
+        veiculo.setId(1L);
+        veiculo.setCor(cor);
+        veiculo.setModelo(modelo);
+        veiculo.setTipoVeiculo(tipoVeiculo);
+        veiculo.setPlaca("BRA2E19");
+
+        System.out.println(veiculo.exibirDadosVeiculoDetalhado());
+
+    }
 }
